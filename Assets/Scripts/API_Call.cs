86568cb7dec5b38d0ecba85847f8db68d0995018
Bedaprakash Saidarshan Sahoo <145172881+VedprakashSaidarshanSahoo@users.noreply.github.com>
2024-05-21 21:28:19 +0530

    Debugged Some Issues Related To Build

--- Assets/Scripts/API_Call.cs
+++ Assets/Scripts/API_Call.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class API_Call : MonoBehaviour
